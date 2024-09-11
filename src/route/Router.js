eb59b326030f710fab13ee63ad9bd2c8e118e750
bilalseen <bilalsen2010@gmail.com>
2024-09-11 16:48:54 +0300

    Refactor Router component to add JobDetail screen

--- src/route/Router.js
+++ src/route/Router.js
@@ -1,18 +1,20 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Drawer from "./Drawer";
+import JobDetail from "../screens/JobDetail";
 
 const Stack = createNativeStackNavigator();
 
 export default Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Drawer" component={Drawer} />
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Drawer"
+          component={Drawer}
+        />
+        <Stack.Screen name="JobDetail" component={JobDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
