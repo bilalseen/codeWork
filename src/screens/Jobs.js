8895145f7f1b2f4e5f207ab3c202fbd0995a471d
bilalseen <bilalsen2010@gmail.com>
2024-09-11 16:49:53 +0300

    Refactor Jobs screen component to fetch and display job data

--- src/screens/Jobs.js
+++ src/screens/Jobs.js
@@ -1,10 +1,44 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import JobCard from "../components/JobCard";
 
-export default function Jobs() {
+export default function Jobs({ navigation }) {
+  const API_URL = "https://www.themuse.com/api/public/jobs?page=1";
+  const [data, loading, error] = useFetch(API_URL);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Jobs</Text>
+      <FlatList
+        data={data.results}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <JobCard item={item} navigation={navigation} />
+        )}
+      />
     </View>
   );
 }
