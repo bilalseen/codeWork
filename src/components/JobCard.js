552506d7c71af356e480f2e4684276f00bb844f0
bilalseen <bilalsen2010@gmail.com>
2024-09-14 17:16:02 +0300

    Refactor JobCard component to display favorite job indicator

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,14 +1,37 @@
-import { Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
-import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Heart } from "lucide-react-native";
+import { useSelector } from "react-redux";
 
 export default function JobCard({ item, navigation }) {
+  const [isItFavorite, setFavorite] = useState(false);
+  const favoriteJobs = useSelector((state) => state.favoriteJob.favoriteJob);
+
   const handleNavigateDetail = () => {
     navigation.navigate("JobDetail", { itemID: item.id });
   };
 
+  useEffect(() => {
+    const isFavorite = favoriteJobs.some((job) => job.id === item.id);
+    setFavorite(isFavorite);
+  }, [favoriteJobs, item]);
+
   return (
     <TouchableOpacity style={styles.container} onPress={handleNavigateDetail}>
-      <Text style={styles.jobName}>{item.name}</Text>
+      <View style={styles.row}>
+        <Text style={styles.jobName}>{item.name}</Text>
+        {isItFavorite && (
+          <View style={styles.iconContainer}>
+            <Heart size={24} color={"#EF5350"} fill={"#EF5350"} />
+          </View>
+        )}
+      </View>
       <Text style={styles.jobType}>{item.type}</Text>
       <Text style={styles.locationTag}>{item.locations[0].name}</Text>
       <Text style={styles.levelText}>{item.levels[0].name}</Text>
@@ -20,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     width: Dimensions.get("window").width * 0.9,
-    height: Dimensions.get("window").width * 0.3,
     marginVertical: 10,
     marginHorizontal: 5,
     borderColor: "gray",
@@ -30,8 +52,19 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     gap: 5,
   },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   jobName: {
     fontWeight: "500",
+    flex: 10,
   },
   jobType: {},
   locationTag: {
